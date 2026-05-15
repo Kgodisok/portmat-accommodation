5759db61ca38eb7e0f4d78d7256c7655e4eeeea4
Matsepe Kgodiso <kgodisomatsepe474@mail.com>
2026-05-15 22:40:54 +0200

    Initial Commit

---
+++ mainModule.js
@@ -0,0 +1,14 @@
+const localData = require('./accommodationData.json');
+const express = require('express');
+
+
+const app = express();
+const port = process.env.PORT || 2003;
+
+app.get('/portmat/accommodations', (req, res) => {
+  res.json(localData);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
